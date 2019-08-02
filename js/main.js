7ee4232a1b758157b3bfbc83983dd938f8d41852
じゅえり <supirs102@gmail.com>
2019-08-03 01:34:11 +0900

    jQuery ScrollifyをCDNにて導入も動かず

--- js/main.js
+++ js/main.js
@@ -16,4 +16,22 @@ $(function(){
 		logoResize();
 	});
 
+	//１ページスクロール
+	$.scrollify({section:".box"});
+
+	var current;
+	$.scrollify({
+	    section:".box",
+	    setHeights:false,
+	    before:function(i,box){
+	        current = i;
+	    },
+	});
+	$(window).on('resize',function(){
+	    if(current){
+	        var currentScrl = $('.box').eq(current).offset().top;
+	        $(window).scrollTop(currentScrl);
+	    }
+	});
+
 })
