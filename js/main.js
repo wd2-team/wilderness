9c80d2815bad88dd96fc9ad45656647f4c98276e
じゅえり <supirs102@gmail.com>
2019-08-03 17:31:09 +0900

    わがままサイト 後日変更 やっぱりしない

--- js/main.js
+++ js/main.js
@@ -7,9 +7,24 @@ $(function(){
 		var logoWidth = $('#logoimg').width();
 		var logoHeight = $('#logoimg').height();
 		$('.logo').css({
-			"top": h / 2 - logoHeight / 2,
+			"top": h / 2 - logoHeight / 2 - 30,
 			"left": w / 2 - logoWidth / 2
 		});
+		if (w <= 1440) {
+			$('.logo').css({
+				"top": -30,
+				"left": -50
+			});
+		}
+		if (w <= 480) {
+			$('.logo').css({
+				"top": 0,
+				"left": w / 2 - logoWidth / 2
+			});
+			$('#about-sp').css({
+				"top": logoHeight
+			});
+		}
 	}
 	logoResize();
 	$(window).on('resize', function(){
@@ -17,21 +32,21 @@ $(function(){
 	});
 
 	//１ページスクロール
-	$.scrollify({section:".box"});
+	// $.scrollify({section:".box"});
 
-	var current;
-	$.scrollify({
-	    section:".box",
-	    setHeights:false,
-	    before:function(i,box){
-	        current = i;
-	    },
-	});
-	$(window).on('resize',function(){
-	    if(current){
-	        var currentScrl = $('.box').eq(current).offset().top;
-	        $(window).scrollTop(currentScrl);
-	    }
-	});
+	// var current;
+	// $.scrollify({
+	//     section:".box",
+	//     setHeights:false,
+	//     before:function(i,box){
+	//         current = i;
+	//     },
+	// });
+	// $(window).on('resize',function(){
+	//     if(current){
+	//         var currentScrl = $('.box').eq(current).offset().top;
+	//         $(window).scrollTop(currentScrl);
+	//     }
+	// });
 
 })
